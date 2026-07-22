238a0f711f268503c24a6bdd098b8ade578ece22
Poomfy Memmy! <nayracutie7+softy@proton.me>
2026-07-22 17:17:43 +0100

    Update userscript.js

--- tweaks/fa-plus/userscript.js
+++ tweaks/fa-plus/userscript.js
@@ -1,1 +1,21 @@
+// ==UserScript==
+// @name         FurAffinity[dot]net +
+// @version      b0.0
+// @description  Maximising the best out of FurAffinity[dot]net, by removing advertisements and features that bottleneck the experience.
+// @author       SoftyBnuny
+// @match        *://*.furaffinity.net/*
+// @run-at       document-start
+// @icon         https://softybnuny.github.io/tweaks/fa-plus/faplus-icon.ico
+// @grant        GM_addStyle
+// ==/UserScript==
 
+(function() {
+'use strict';
+
+/*mainTweakLoad();
+
+function mainTweakLoad() {
+    GM_addStyle(`
+    `);
+}*/
+})();
